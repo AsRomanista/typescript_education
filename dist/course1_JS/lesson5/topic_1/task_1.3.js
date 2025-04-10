ad6d7bdad52aefb9fd63cd36ed0962da47a27421
AsRomanista <dmitry.garkovitch@gmail.com>
2025-04-10 12:28:03 +0200

    ts: topic 3

---
+++ dist/course1_JS/lesson5/topic_1/task_1.3.js
@@ -0,0 +1,9 @@
+"use strict";
+/*Можно ли изменить объект, объявленный с помощью const? Как вы думаете?*/
+// Ответ: можно
+const passport = {
+    username: 'Smith',
+    surname: 'John'
+};
+passport.username = 'Vitya';
+console.log(passport);
